305d5e689a66a3177a10eaaa778ea2babf481a51
Sawby <130822964+sawby08@users.noreply.github.com>
2025-07-05 16:48:32 -0400

    Split encounter data into separate files

---
+++ encounters/Test enemies/events.lua
@@ -0,0 +1,249 @@
+function eventsSetup(data)
+
+    data.onDeath = function(enemy)       -- Function that's run when an enemy dies (enemy is the enemy that's dead)
+        if enemy == 1 then
+            battle.turnCount = 1
+            encounter.attacks = {}
+            encounter.attacks[1] = {
+                boxDims = {
+                    x = math.floor(320 - 135/2),
+                    y = 253,
+                    width = 135,
+                    height = 135
+                },
+                dialogue = {
+                    {
+                        speaker = 2,
+                        text = "heh. i don't believe\nin you anymore..",
+                        bubbleDirection = "left",
+                        bubbleOffset = 0,
+                        font = fonts.dialogue
+                    }
+                },
+                init = function()
+                    -- Init attack stuff (don't worry about this)
+                    attackTimer = 0
+                    timeSince = 0
+                    bullets = {}
+                    player.mode = 2
+                    -- Everything below this is your custom code
+                end,
+                update = function(dt)
+                    -- Update attack stuff (don't worry about this)
+                    timeSince = timeSince + 1 * dt*30
+                    if timeSince >= 1 then
+                        attackTimer = attackTimer + 1
+                        timeSince = 0
+                    end
+
+                    local i = 1 -- here to remove bullets
+                    for _, bullet in ipairs(bullets) do
+                        bullet:update(dt)
+                        if bullet.remove then
+                            table.remove(bullets, i)
+                        end
+                        i = i + 1
+                    end
+                    -- Everything below this is your custom code
+
+                    if attackTimer == 15 then
+                        local battleEngine = require 'source.battleEngineState'
+                        battleEngine.changeBattleState('buttons', 'player')
+                        bullets = {}
+                    end
+                end,
+                draw = function()
+                end
+            }
+            encounter.text = "[clear]shut up dude"
+        elseif enemy == 2 then
+            battle.turnCount = 1
+            encounter.bgm:setPitch(0.25)
+            encounter.attacks = {
+                {
+                    boxDims = {
+                        x = math.floor(320 - 135/2),
+                        y = 253,
+                        width = 135,
+                        height = 135
+                    },
+                    dialogue = {
+                        {
+                            speaker = 1,
+                            text = "[shake]oh... :-(",
+                            bubbleDirection = "right",
+                            bubbleOffset = 125,
+                            font = fonts.dialogue
+                        }
+                    },
+                    init = function()
+                        -- Init attack stuff (don't worry about this)
+                        attackTimer = 0
+                        timeSince = 0
+                        bullets = {}
+                        player.mode = 2
+                        -- Everything below this is your custom code
+                    end,
+                    update = function(dt)
+                        -- Update attack stuff (don't worry about this)
+                        timeSince = timeSince + 1 * dt*30
+                        if timeSince >= 1 then
+                            attackTimer = attackTimer + 1
+                            timeSince = 0
+                        end
+
+                        local i = 1 -- here to remove bullets
+                        for _, bullet in ipairs(bullets) do
+                            bullet:update(dt)
+                            if bullet.remove then
+                                table.remove(bullets, i)
+                            end
+                            i = i + 1
+                        end
+                        -- Everything below this is your custom code
+
+                        if attackTimer == 15 then
+                            local battleEngine = require 'source.battleEngineState'
+                            battleEngine.changeBattleState('buttons', 'player')
+                            bullets = {}
+                        end
+                    end,
+                    draw = function()
+                    end
+                    }
+                }
+            encounter.text = "[clear]* You feel an overwhelming\n  sense of regret."
+            encounter.enemies[1].canSpare = true
+            encounter.enemies[1].canDodge = false
+            encounter.enemies[1].showHPBar = true
+            encounter.enemies[1].defense = -99
+            encounter.enemies[1].description = "* Devoid of anything without\n  their friend."
+            encounter.enemies[1].acts = {}
+        end
+    end
+
+    data.onSpare = function(enemy)
+        if enemy == 1 then
+            battle.turnCount = 1
+            encounter.attacks = {}
+            encounter.attacks[1] = {
+                boxDims = {
+                    x = math.floor(320 - 135/2),
+                    y = 253,
+                    width = 135,
+                    height = 135
+                },
+                dialogue = {
+                    {
+                        speaker = 2,
+                        text = "this is cool i\nguess",
+                        bubbleDirection = "left",
+                        bubbleOffset = 0,
+                        font = fonts.dialogue
+                    }
+                },
+                init = function()
+                    -- Init attack stuff (don't worry about this)
+                    attackTimer = 0
+                    timeSince = 0
+                    bullets = {}
+                    player.mode = 2
+                    -- Everything below this is your custom code
+                end,
+                update = function(dt)
+                    -- Update attack stuff (don't worry about this)
+                    timeSince = timeSince + 1 * dt*30
+                    if timeSince >= 1 then
+                        attackTimer = attackTimer + 1
+                        timeSince = 0
+                    end
+
+                    local i = 1 -- here to remove bullets
+                    for _, bullet in ipairs(bullets) do
+                        bullet:update(dt)
+                        if bullet.remove then
+                            table.remove(bullets, i)
+                        end
+                        i = i + 1
+                    end
+                    -- Everything below this is your custom code
+
+                    if attackTimer == 15 then
+                        local battleEngine = require 'source.battleEngineState'
+                        battleEngine.changeBattleState('buttons', 'player')
+                        bullets = {}
+                    end
+                end,
+                draw = function()
+                end
+            }
+            encounter.text = "* Enemy 2 is filled with\n  conptemtness."
+            encounter.enemies[2].canSpare = true
+            encounter.enemies[2].canDodge = false
+            encounter.enemies[2].showHPBar = true
+            encounter.enemies[2].acts = {}
+
+        elseif enemy == 2 then
+            battle.turnCount = 1
+            encounter.attacks = {
+                {
+                    boxDims = {
+                        x = math.floor(320 - 135/2),
+                        y = 253,
+                        width = 135,
+                        height = 135
+                    },
+                    dialogue = {
+                        {
+                            speaker = 1,
+                            text = ":-) !!",
+                            bubbleDirection = "right",
+                            bubbleOffset = 125,
+                            font = fonts.dialogue
+                        }
+                    },
+                    init = function()
+                        -- Init attack stuff (don't worry about this)
+                        attackTimer = 0
+                        timeSince = 0
+                        bullets = {}
+                        player.mode = 2
+                        -- Everything below this is your custom code
+                    end,
+                    update = function(dt)
+                        -- Update attack stuff (don't worry about this)
+                        timeSince = timeSince + 1 * dt*30
+                        if timeSince >= 1 then
+                            attackTimer = attackTimer + 1
+                            timeSince = 0
+                        end
+
+                        local i = 1 -- here to remove bullets
+                        for _, bullet in ipairs(bullets) do
+                            bullet:update(dt)
+                            if bullet.remove then
+                                table.remove(bullets, i)
+                            end
+                            i = i + 1
+                        end
+                        -- Everything below this is your custom code
+
+                        if attackTimer == 15 then
+                            local battleEngine = require 'source.battleEngineState'
+                            battleEngine.changeBattleState('buttons', 'player')
+                            bullets = {}
+                        end
+                    end,
+                    draw = function()
+                    end
+                    }
+                }
+            encounter.text = "[clear]* Enemy 1 is enjoying themself."
+            encounter.enemies[1].canSpare = true
+            encounter.enemies[1].canDodge = false
+            encounter.enemies[1].showHPBar = true
+            encounter.enemies[1].acts = {}
+        end
+    end
+    
+end
---
+++ encounters/Test enemies/waves.lua
@@ -0,0 +1,247 @@
+-- Don't edit these!
+local timeSince = 0
+local bullets = {}
+local attackTimer = 0
+
+function wavesSetup(data)
+    exampleBullet = require(data.encounterPath .. 'bullets/example')
+
+    data.attacks = {
+        {
+            boxDims = {
+                x = math.floor(320 - 135/2),
+                y = 253,
+                width = 135,
+                height = 135
+            },
+            dialogue = {
+                {
+                    speaker = 1,
+                    text = "hey lads",
+                    bubbleDirection = "right",
+                    bubbleOffset = 125,
+                    font = fonts.dialogue
+                },
+                {
+                    speaker = 1,
+                    text = "how we doin today",
+                    bubbleDirection = "right",
+                    bubbleOffset = 125,
+                    font = fonts.sans
+                },
+                {
+                    speaker = 2,
+                    text = "SHUT YOUR FUCKING\nMOUTH!!!!!",
+                    bubbleDirection = "left",
+                    bubbleOffset = 0,
+                    font = fonts.papyrus
+                }
+            },
+            init = function()
+                -- Init attack stuff (don't worry about this and don't remove it)
+                attackTimer = 0
+                timeSince = 0
+                bullets = {}
+                player.mode = 1
+                -- Everything below this is your custom code
+            end,
+            update = function(dt)
+                -- Update attack stuff (don't worry about this and don't remove it)
+                timeSince = timeSince + 1 * dt*30
+                if timeSince >= 1 then
+                    attackTimer = attackTimer + 1
+                    timeSince = 0
+                end
+
+                local i = 1 -- here to remove bullets
+                for _, bullet in ipairs(bullets) do
+                    bullet:update(dt)
+                    if bullet.remove then
+                        table.remove(bullets, i)
+                    end
+                    i = i + 1
+                end
+                -- Everything below this is your custom code
+
+                if attackTimer == 15 then
+                    table.insert(
+                        bullets,
+                        exampleBullet:create(231, 312, 4, 0, "white", true, 2)
+                    )
+                    table.insert(
+                        bullets,
+                        exampleBullet:create(312 + (312 - 231), 312, -4, 0, "white", true, 2)
+                    )
+                    table.insert(
+                        bullets,
+                        exampleBullet:create(312, 253-24, 0, 4, "white", true, 2)
+                    )
+                    table.insert(
+                        bullets,
+                        exampleBullet:create(312, 253 + 135 + 8, 0, -4, "white", true, 2)
+                    )
+                    attackTimer = 16    -- Prevents multiple insances of an attack at once
+                end
+                if attackTimer == 30 then
+                    for i=1, 16 do
+                        table.insert(
+                            bullets,
+                            exampleBullet:create(231 + i*17, 253 + 135 + 8, 0, -4, "orange", true, 2)
+                        )
+                    end
+                    for i=1, 16 do
+                        table.insert(
+                            bullets,
+                            exampleBullet:create(231 + i*17, 253 - 24, 0, 4, "blue", true, 2)
+                        )
+                    end
+                    table.insert(
+                        bullets,
+                        exampleBullet:create(231, 253-24, 4, 4, "white", true, 2)
+                    )
+                    table.insert(
+                        bullets,
+                        exampleBullet:create(312 + (312 - 231), 253-24, -4, 4, "white", true, 2)
+                    )
+                    table.insert(
+                        bullets,
+                        exampleBullet:create(312, 253 + 135 + 8, 0, -4, "white", true, 2)
+                    )
+                    attackTimer = 31
+                end
+
+                if attackTimer == 70 then
+                    local battleEngine = require 'source.battleEngineState'
+                    battleEngine.changeBattleState('buttons', 'player')
+                    bullets = {}
+                end
+            end,
+            draw = function()
+                -- Draw attack stuff (don't worry about this and don't remove it)
+                for _, bullet in ipairs(bullets) do
+                    bullet:draw()
+                end
+            end
+        },
+
+        {
+            boxDims = {
+                x = math.floor(320 - 200/2),
+                y = 203,
+                width = 200,
+                height = 185
+            },
+            dialogue = {
+                {
+                    speaker = 1,        -- Enemy number from the enemies array
+                    text = "why are you saying\nsuch rude things\nto me",
+                    bubbleDirection = "right",
+                    bubbleOffset = 125,
+                    font = fonts.dialogue
+                },
+                {
+                    speaker = 2,
+                    text = "shut up",
+                    bubbleDirection = "left",
+                    bubbleOffset = 0,
+                    font = fonts.dialogue
+                }
+            },
+            init = function()
+                -- Init attack stuff (don't worry about this)
+                attackTimer = 0
+                timeSince = 0
+                bullets = {}
+                player.mode = 2
+                -- Everything below this is your custom code
+            end,
+            update = function(dt)
+                -- Update attack stuff (don't worry about this)
+                timeSince = timeSince + 1 * dt*30
+                if timeSince >= 1 then
+                    attackTimer = attackTimer + 1
+                    timeSince = 0
+                end
+
+                local i = 1 -- here to remove bullets
+                for _, bullet in ipairs(bullets) do
+                    bullet:update(dt)
+                    if bullet.remove then
+                        table.remove(bullets, i)
+                    end
+                    i = i + 1
+                end
+                -- Everything below this is your custom code
+
+                if attackTimer == 15 then
+                    local rand1 = love.math.random(1, 12)
+                    local rand2 = love.math.random(1, 12)
+                    for i=1, 12 do
+                        if i ~= rand1 and i ~= rand2 then
+                            table.insert(
+                                bullets,
+                                exampleBullet:create(204 + i*17, 187, 0, 4, "white", true, 2)
+                            )
+                            attackTimer = 16
+                        end
+                    end
+                end
+
+                if attackTimer == 30 then
+                    local rand1 = love.math.random(1, 12)
+                    local rand2 = love.math.random(1, 12)
+                    for i=1, 12 do
+                        if i ~= rand1 and i ~= rand2 then
+                            table.insert(
+                                bullets,
+                                exampleBullet:create(204 + i*17, 187, 0, 4, "white", true, 2)
+                            )
+                            attackTimer = 31
+                        end
+                    end
+                end
+
+                if attackTimer == 45 then
+                    local rand1 = love.math.random(1, 12)
+                    local rand2 = love.math.random(1, 12)
+                    for i=1, 12 do
+                        if i ~= rand1 and i ~= rand2 then
+                            table.insert(
+                                bullets,
+                                exampleBullet:create(204 + i*17, 187, 0, 4, "white", true, 2)
+                            )
+                            attackTimer = 46
+                        end
+                    end
+                end
+
+                if attackTimer == 60 then
+                    local rand1 = love.math.random(1, 12)
+                    local rand2 = love.math.random(1, 12)
+                    for i=1, 12 do
+                        if i ~= rand1 and i ~= rand2 then
+                            table.insert(
+                                bullets,
+                                exampleBullet:create(204 + i*17, 187, 0, 4, "white", true, 2)
+                            )
+                            attackTimer = 61
+                        end
+                    end
+                end
+
+                if attackTimer == 100 then
+                    local battleEngine = require 'source.battleEngineState'
+                    battleEngine.changeBattleState('buttons', 'player')
+                    bullets = {}
+                end
+            end,
+            draw = function()
+                -- Draw attack stuff (don't worry about this)
+                for _, bullet in ipairs(bullets) do
+                    bullet:draw()
+                end
+                -- Everything below this is your custom code
+            end
+        }
+    }
+end
